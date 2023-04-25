96031757a98b68c417a5c5d38d256db2ba3df6ec
Gimjeounghui <ealiyel@naver.com>
2023-04-25 16:00:19 +0900

    메뉴 중 업무 추가

---
+++ eis/src/main/java/com/kun/eis/work/controller/WorkController.java
@@ -0,0 +1,125 @@
+package com.kun.eis.work.controller;
+
+import java.util.HashMap;
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.kun.eis.common.util.BoardUtil;
+import com.kun.eis.work.service.WorkService;
+import com.kun.eis.work.vo.WorkVO;
+
+@Controller
+public class WorkController {
+
+	private static final Logger logger = LoggerFactory.getLogger(WorkController.class);
+	
+	@Autowired
+	private WorkService workService;
+	
+	@RequestMapping(value="/")
+	public String testHome(@ModelAttribute("searchVO") WorkVO vo, Model model, HttpSession session) throws Exception {	
+		
+		return "/test/testHome.tiles";
+	}
+	
+	/**
+	 * test항목 검색Family Site
+	 * 
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping(value="/testList")
+	public String testList(@ModelAttribute("searchVO") WorkVO vo, Model model, HttpSession session) throws Exception {	
+		
+		// 게시판 UTIL
+		BoardUtil boardUtil = new BoardUtil();
+		List<WorkVO> testList = null;
+		
+		try {
+			
+			/**
+    	    * 게시판 기능
+    	    */
+    		int totalRecordCount = 0; 								// 총 게시물 건수
+    		int currentPageNo = vo.getCurrentPageNo(); 				// 현재 클릭한 page 번호
+    		int pageSize = vo.getPageSize(); 						// 페이지 리스트에 게시되는 페이지 건수
+    		int recordCountPerPage = vo.getRecordCountPerPage();	// 한 페이지당 게시되는 게시물 건 수 
+    		
+    		// 게시물 조회 범위 연산
+    		HashMap<String, Integer> rangeMap = boardUtil.calcDataRange(currentPageNo, recordCountPerPage);
+    		vo.setStart(rangeMap.get("firstRecordIndex"));
+    		vo.setEnd(rangeMap.get("lastRecordIndex"));
+    		
+			// 전체 검색 결과
+			testList = workService.testList(vo);
+			
+			// 검색된 결과가 있는지 체크
+    		if(testList.size() > 0){
+    			// 전체 검색 결과 게시물 건 수
+    			totalRecordCount = testList.get(0).getTotalRecordCount();
+    		}
+			
+    		// pager 기능 모든 계산식 결과 정보 map에 담기
+    		HashMap<String, Integer> pagerMap = boardUtil.calcBoardPagerElement(currentPageNo, totalRecordCount, recordCountPerPage, pageSize);
+    		/**
+     	    * 게시판 END
+     	    */
+    		
+    		// model 세팅
+			model.addAttribute("testList", testList ); 	// 검색한 list정보
+			model.addAttribute("pagerMap", pagerMap);	// 게시판 계산식 map
+			
+		} catch (Exception e) {
+			
+			logger.info(e.getMessage()); 
+			e.printStackTrace(); 
+		}
+		
+		return "/test/test.tiles";
+	}
+	
+	/**
+	 * ajax test
+	 * 
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping("/ajax_testAjax.ajax")
+	@ResponseBody
+	public Object procEqpmntManage(@ModelAttribute("vo") WorkVO vo, Model model, HttpSession session) throws Exception {	
+		
+		HashMap<String, Object> resultMap = new HashMap<String, Object>();
+		// 결과 성공여부
+		boolean rstFlag = false;
+		WorkVO workVO = null;
+		
+		try {
+		    			
+			workVO = workService.selectPwCode(vo);
+			rstFlag = true;
+			
+		} catch (Exception e) {
+			
+			rstFlag = false;
+			logger.debug(e.getMessage());
+			e.printStackTrace();
+			
+		} finally {
+		
+			resultMap.put("rstFlag", rstFlag);
+			resultMap.put("workVO", workVO);
+		}
+		
+		return resultMap;
+	}
+}
